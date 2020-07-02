d85c6dc6af00f65da99c4d730e0b40815c0a8c0d
Ander Gray <ander.gray@liverpool.ac.uk>
2020-07-02 21:51:15 +0100

    release ready

--- Hvolume.m
+++ Hvolume.m
@@ -2,7 +2,7 @@ function volume = Hvolume(varargin)
     %%%
     %   Calculates the H-volume of a measure in some n-box
     %   
-    %   Vol = Hvolume(measure,J1,J2,...,Jn)
+    %   Vol = Hvolume(measure, J1, J2,..., Jn)
     %
     %   measure is an anonymous function of dimension n and J1 to Jn are
     %   n intervals defining the n-box.
@@ -41,7 +41,7 @@ function volume = Hvolume(varargin)
     %%%
     
     nVarg = length(varargin);       % Number of inputs
-    nDims = nVarg -1;               % Number of dimensions
+    nDims = nVarg - 1;              % Number of dimensions
     
     measure = varargin{1};
     Js = zeros(nDims,2);
@@ -50,7 +50,6 @@ function volume = Hvolume(varargin)
     end
     
     Nvertix = 2^nDims;
-    
     vertices = CartProduct(Js);        % Compute Cartesian product with intervals
     
     if length(vertices) ~= Nvertix
@@ -58,18 +57,17 @@ function volume = Hvolume(varargin)
     end
     
     measureVals = zeros(Nvertix,1);
-    
     for i = 1:Nvertix
-        measureVals(i) = measure(vertices(i,:));      % Eval vertex in measure
+        measureVals(i) = measure(vertices(i,:));    % Eval vertex in measure
         
         Ns = 0;
         for j=1:nDims
             Ns = Ns + vertices(i,j) == Js(j,1);     % Find how many vertices are lower bounds
-        end                                           % Can someone please do this vectoriesed?
+        end                                         % Can someone please do this vectoriesed?
         
         sign = 1;
-        if mod(Ns,2) == 1                             % If number of lower bounds are odd
-            sign = -1;                                % measure value will be subtracted
+        if mod(Ns,2) == 1                           % If number of lower bounds are odd
+            sign = -1;                              % measure value will be subtracted
         end
         measureVals(i) = sign * measureVals(i);
     end
@@ -78,6 +76,9 @@ function volume = Hvolume(varargin)
 end
 
 
+%%%
+% Carteesian product function for N intervals. JS is a matrix of endpoints
+%%%
 function verticies = CartProduct(Js)
    
     Ndims = size(Js,1);
